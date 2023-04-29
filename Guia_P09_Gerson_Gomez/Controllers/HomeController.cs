b3c6ed7d1250daf25dad338fce4b471565aeeba1
Gerson Gomez <ger.gomezz56@gmail.com>
2023-04-29 10:42:01 -0600

    all ok

--- Guia_P09_Gerson_Gomez/Controllers/HomeController.cs
+++ Guia_P09_Gerson_Gomez/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace Guia_P09_Gerson_Gomez.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller 
     {
         private readonly ILogger<HomeController> _logger;
 
